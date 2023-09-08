c993b3ee059cf3d664ab32f65ca104acd65dd848
Roberto Cunha <robertocunha@gmail.com>
2023-09-07 21:05:36 -0300

    [Feat]Destaques obedecem a ordem correta

--- script.js
+++ script.js
@@ -5,59 +5,87 @@ const stepForwardButton = document.getElementById("stepForwardButton");
 const codeElement = document.querySelector(".code-container code");
 
 let animationInterval;
-let currentIndex = 0;
-const totalSteps = 10; // número de linhas menos 1
 
-highlightCodeLine(currentIndex);
+const unOrderedElments = [100, 80, 120];
+const steps = generateSteps(unOrderedElments);
+let stepsIndex = 0;
+let currentStep = steps[stepsIndex];
+const totalSteps = steps.length;
+
+
+function generateSteps(arr) {
+    const roteiro = [];
+    const len = arr.length;
+
+    roteiro.push(0);
+    for (let i = 0; i < len -1; i++) {
+        roteiro.push(1);
+        for (let j = i + 1; j < len; j++) {
+            roteiro.push(2, 3)
+            if (arr[i] > arr[j]) {
+                const temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                roteiro.push(4, 5, 6);
+            }
+            roteiro.push(7, 8)
+            if (j === len) roteiro.push(2, 8);
+        }
+        roteiro.push(9);
+        if (i === len - 1) roteiro.push(1, 9);
+    }
+    roteiro.push(10);
+    return roteiro;
+}
 
 function highlightCodeLine(index) {
     const codeLines = document.querySelectorAll(".code-line");
-
+    
     codeLines.forEach(line => {
         line.classList.remove("highlighted");
     });
-
+    
     codeLines[index].classList.add("highlighted");
 }
 
 function pauseAnimation() {
+    playButton.disabled = false;
+    
     clearInterval(animationInterval);
 }
 
 function playAnimation() {
+    playButton.disabled = true;
+    
     animationInterval = setInterval(() => {
-        if (currentIndex === totalSteps) {
-            currentIndex = 0;
-        } else {
-            currentIndex++;
-        }
-        highlightCodeLine(currentIndex);
+        stepsIndex = (stepsIndex + 1) % totalSteps;
+        
+        currentStep = steps[stepsIndex];
+        highlightCodeLine(currentStep);
     }, 1000);
 }
 
 function stepForward() {
     pauseAnimation();
     
-    if (currentIndex === totalSteps) {
-        currentIndex = 0;
-    } else {
-        currentIndex++;
-    }
-    highlightCodeLine(currentIndex);
+    stepsIndex = (stepsIndex + 1) % totalSteps;
+    
+    currentStep = steps[stepsIndex];
+    highlightCodeLine(currentStep);
 }
 
 function stepBackward() {
     pauseAnimation();
     
-    if (currentIndex === 0) {
-        currentIndex = totalSteps;
-    } else {
-        currentIndex--;
-    }
-    highlightCodeLine(currentIndex);
+    stepsIndex = (stepsIndex - 1 + totalSteps) % totalSteps;
+    
+    currentStep = steps[stepsIndex];
+    highlightCodeLine(currentStep);
 }
 
 stepBackwardButton.addEventListener("click", stepBackward);
 pauseButton.addEventListener("click", pauseAnimation);
 playButton.addEventListener("click", playAnimation);
 stepForwardButton.addEventListener("click", stepForward);
+
+highlightCodeLine(currentStep);
